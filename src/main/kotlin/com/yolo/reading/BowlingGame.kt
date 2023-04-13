0295f5716823a2ece23d31e4eb7b93c79e3ca275
Pengrui.Zhao <pengrui.zhao@thoughtworks.com>
2023-04-13 20:38:28 +0800

    should return 3 when hit 1 and 2

--- src/main/kotlin/com/yolo/reading/BowlingGame.kt
+++ src/main/kotlin/com/yolo/reading/BowlingGame.kt
@@ -1,11 +1,13 @@
 package com.yolo.reading
 
 class BowlingGame {
+    private var scores = 0
     fun hit(bowl: Int) {
+        scores += bowl
     }
 
     fun getScoreForFirstFrame(): Int {
-        return 0
+        return scores
     }
 
 }
--- src/test/kotlin/com/yolo/reading/BowlingGameTests.kt
+++ src/test/kotlin/com/yolo/reading/BowlingGameTests.kt
@@ -13,4 +13,13 @@ class BowlingGameTests {
             assertEquals(0, this.getScoreForFirstFrame())
         }
     }
+
+    @Test
+    fun `should return 3 when hit 1 and 2`() {
+        BowlingGame().run {
+            this.hit(bowl = 1)
+            this.hit(bowl = 2)
+            assertEquals(3, this.getScoreForFirstFrame())
+        }
+    }
 }
