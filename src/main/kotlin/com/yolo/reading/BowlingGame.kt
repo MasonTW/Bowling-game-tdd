1ba58126034ad2fbb814746e6f1ddd96d1b3e947
MasonTW <mingxin.wang@thoughtworks.com>
2023-04-14 18:05:54 +0800

    add null to bowls when Strike

--- src/main/kotlin/com/yolo/reading/BowlingGame.kt
+++ src/main/kotlin/com/yolo/reading/BowlingGame.kt
@@ -1,21 +1,40 @@
 package com.yolo.reading
-
 class BowlingGame {
-    private var bowls: MutableList<Int> = mutableListOf()
+    private var bowls: MutableList<Int?> = mutableListOf()
     fun hit(bowl: Int) {
         bowls.add(bowl)
+        if (bowl == 10){
+            bowls.add(null)
+        }
     }
 
     fun getScoreByFrame(frame: Int): Int {
         val startIndex = getIndexByFrame(frame)
         if (isStrike(frame) || isSpare(frame)) {
-            return bowls[startIndex] + bowls[startIndex + 1] + bowls[startIndex + 2]
+            return getStrikeOrSpareScore(startIndex)
         }
-        return bowls[startIndex] + bowls[startIndex + 1]
+        return bowls[startIndex]!! + bowls[startIndex + 1]!!
     }
 
+    private fun getStrikeOrSpareScore(startIndex: Int): Int {
+        var sum = 0
+        var count = 0
+        var index = startIndex
+        while (count < 3 && index < bowls.size) {
+            bowls[index].let {
+                if (it != null) {
+                    sum += it
+                    count++
+                }
+                index++
+            }
+        }
+        return sum
+    }
+
+
     private fun isSpare(frame: Int) = getIndexByFrame(frame).run {
-        bowls[this] + bowls[this + 1] == 10
+        bowls[this]!! + bowls[this + 1]!! == 10
     }
 
     private fun getIndexByFrame(frame: Int) = COUNT_OF_EACH_FRAME * (frame - 1)
@@ -29,3 +48,4 @@ class BowlingGame {
     }
 
 }
+
